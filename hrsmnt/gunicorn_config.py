e00f22db2e3252f72ea468b7537ca951105988b3
Mark Bykhovets <mb@hrsmnt.ru>
2020-07-01 09:20:19 +0000

    gunicorn settings

---
+++ hrsmnt/gunicorn_config.py
@@ -0,0 +1,8 @@
+command = '/home/mark/test.hrsmnt.ru/env/bin/gunicorn'
+pythonpath = '/home/mark/test.hrsmnt.ru/hrsmnt'
+bind = '127.0.0.1:8064'
+workers = 5
+user = 'mark'
+limit_request_fields = 32000
+limit_request_lield = 0
+raw_env = 'DJANGO_SETTINGS_MODULE=hrsmnt.settings'
